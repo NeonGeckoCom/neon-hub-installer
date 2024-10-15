c7a30282376cd9cc8a7f3d22c68b277dcf7ddae9
Mike <mike@graywind.org>
2024-10-14 21:22:52 -0500

    drop windows command

--- scripts/common.sh
+++ scripts/common.sh
@@ -141,7 +141,6 @@ function create_python_venv() {
 function install_ansible() {
     echo -ne "➤ Installing Ansible requirements in Python virtualenv... "
     ANSIBLE_VERSION="9.2.0"
-    [ "$(ver "$PYTHON")" -lt "$(ver 3.10)" ] && ANSIBLE_VERSION="8.7.0"
     $PIP_COMMAND install ansible=="$ANSIBLE_VERSION" docker==7.1.0 requests==2.31.0 &>>"$LOG_FILE"
     ansible-galaxy collection install -r ansible/requirements.yml &>>"$LOG_FILE"
     echo -e "[$done_format]"
