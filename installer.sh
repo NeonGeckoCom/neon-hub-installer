7e16c13a9d2c86e6c3e5708f5ebc6a7c4d7ff057
mikejgray <mike@graywind.org>
2024-11-18 22:08:21 -0600

    feat: install neon node client

--- installer.sh
+++ installer.sh
@@ -110,12 +110,26 @@ case $HOSTNAME_CHOICE in
         ;;
 esac
 
+INSTALL_NODE_VOICE_CLIENT_CHOICE=$(whiptail --title "Neon Hub Installer" --menu "Install the Neon Node Voice Client? This allows you to treat your Hub as a Node." 15 78 3 \
+"1" "Yes (default)" \
+"2" "No" 3>&1 1>&2 2>&3)
+
+case $INSTALL_NODE_VOICE_CLIENT_CHOICE in
+    1)
+        INSTALL_NODE_VOICE_CLIENT=1
+        ;;
+    2)
+        INSTALL_NODE_VOICE_CLIENT=0
+        ;;
+esac
+
 # Installation
 echo "Installing Neon Hub. This may take some time, take a break and relax."
 echo "You can find installation logs at $LOG_FILE."
 
 export ANSIBLE_CONFIG=ansible.cfg
-ansible-playbook -i 127.0.0.1 -e "xdg_dir=$XDG_DIR common_name=$HOSTNAME" "${ansible_debug[@]}" ansible/hub.yaml | tee $ANSIBLE_LOG_FILE
+ansible-playbook -i 127.0.0.1 -e "xdg_dir=$XDG_DIR common_name=$HOSTNAME install_neon_node=$INSTALL_NODE_VOICE_CLIENT" \
+"${ansible_debug[@]}" ansible/hub.yaml | tee $ANSIBLE_LOG_FILE
 
 if [ "${PIPESTATUS[0]}" -eq 0 ]; then
     show_message "Neon Hub has been successfully installed!"
