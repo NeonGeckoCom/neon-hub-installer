2990ec2296eddded80151269e7cac2895b596505
Mike <mike@graywind.org>
2025-05-17 21:35:28 -0500

    enhanced first boot experience

--- debos/overlays/first-boot-hub-setup/usr/local/bin/neon-hub-graphical-wrapper.sh
+++ debos/overlays/first-boot-hub-setup/usr/local/bin/neon-hub-graphical-wrapper.sh
@@ -31,11 +31,31 @@ done
 
 kdialog --title "$DEFAULT_TITLE" --msgbox "Starting Neon Hub setup...\n\nThis will take some time. You will receive a notification when it is complete."
 declare -i RESULT
-RESULT=$(docker compose -f /home/neon/compose/neon-hub.yml up)
+
+docker compose -f /home/neon/compose/neon-hub.yml up -d
+
+# Wait for containers to start, keep waiting until they are running
+SLEEP_INCREMENT=10 # Sleep duration in seconds per check
+# Calculate how many sleep increments make up 5 minutes (300 seconds)
+UPDATE_INTERVAL_COUNT=$(( (5 * 60) / SLEEP_INCREMENT ))
+sleep_cycles_elapsed=0
+
+while true; do
+  if docker ps | grep -q "neon"; then
+    RESULT=0
+    break
+  else
+    sleep $SLEEP_INCREMENT
+    sleep_cycles_elapsed=$((sleep_cycles_elapsed + 1))
+
+    if [ "$sleep_cycles_elapsed" -ge "$UPDATE_INTERVAL_COUNT" ]; then
+      kdialog --title "Neon Hub Setup" --passivepopup "Still waiting for Neon Hub services to start...\n\nThis can take up to an hour or more depending on your internet connection." 60
+      sleep_cycles_elapsed=0 # Reset counter for the next 5-minute interval
+    fi
+  fi
+done
 
 if [ "$RESULT" = "0" ]; then
   kdialog --title "Setup Complete" --msgbox "Your system has been successfully configured!\n\nWelcome to your new Neon Hub.\n\nMore information can be found at https://neongeckocom.github.io/neon-hub-installer/"
   mv /home/neon/.config/autostart/neon-hub-setup.desktop /home/neon/.config/autostart/neon-hub-setup.desktop.done
-else
-  kdialog --title "Setup Warning" --sorry "There was an issue completing setup.\n\nMore information available at $WRAPPER_DEBUG_LOG.\n\nSetup can be run again by restarting the system."
 fi
